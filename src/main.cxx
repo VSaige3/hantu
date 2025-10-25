dfc1d6e8d7fcef550f82ecd436ad8c2be41d3219
torph <73564623+Torphedo@users.noreply.github.com>
2025-10-24 23:27:21 -0400

    Add help & version flags

--- src/main.cxx
+++ src/main.cxx
@@ -1,10 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <memory>
 
 #include <gui_bootstrap.hxx>
 #include <layer_imgui.hxx>
-#include "hantu.hxx"
+#include <common/logging.h>
+#include <common/int.h>
 
-int main() {
+#include "hantu.hxx"
+#include "version.h"
+
+int main(int argc, char** argv) {
+    bool help = false;
+    bool version = false;
+
+    for (u32 i = 0; i < argc; i++) {
+        const char* arg = argv[i];
+        help    |= (strcmp("--help", arg) == 0);
+        version |= (strcmp("--version", arg) == 0);
+    }
+
+    if (version) {
+        printf("%d.%d.%d\n", version_major, version_minor, version_patch);
+        return EXIT_SUCCESS;
+    }
+
+    if (help) {
+        printf("Hantu SSB editor v%d.%d.%d\n", version_major, version_minor, version_patch);
+        printf("Open-source @ %s\n", git_url);
+        printf("Created by:\n");
+        for (const char* author : authors) {
+            printf("  - %s\n", author);
+        }
+        return EXIT_SUCCESS;
+    }
+
     gui_app han;
 
     han.layers.emplace_back(std::make_unique<layer_imgui>());
---
+++ src/version.h
@@ -0,0 +1,8 @@
+#pragma once
+
+static const char* git_url = "https://github.com/VSaige3/hantu";
+static const int version_major = 1;
+static const int version_minor = 0;
+static const int version_patch = 0;
+
+static const char* authors[] = { "Vu", "Torphedo" };
