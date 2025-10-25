a2e7ddf4c58addb78fcdf11b83463fe512bbfbc5
torph <73564623+Torphedo@users.noreply.github.com>
2025-10-24 21:35:50 -0400

    Add basic hardcoded node editor window

--- src/hantu.cxx
+++ src/hantu.cxx
@@ -1,11 +1,38 @@
 #include "hantu.hxx"
 #include <imgui.h>
+#include <common/int.h>
+
+namespace ed = ax::NodeEditor;
 
 void hantu::init(GLFWwindow* window) {
+    editor = ed::CreateEditor();
 }
 
 void hantu::update(GLFWwindow* window) {
     ImGui::ShowDemoWindow(&demo_window_active);
+
+    ImGui::Begin("Test Editor"); {
+        ed::SetCurrentEditor(editor); {
+            ed::Begin("Node Editor", ImVec2());
+
+                u32 id = 1;
+                // Start drawing nodes.
+                ed::BeginNode(id++);
+                    ImGui::Text("Node A");
+                    ed::BeginPin(id++, ed::PinKind::Input);
+                        ImGui::Text("-> In");
+                    ed::EndPin();
+                    ImGui::SameLine();
+                    ed::BeginPin(id++, ed::PinKind::Output);
+                        ImGui::Text("Out ->");
+                    ed::EndPin();
+                ed::EndNode();
+
+            ed::End();
+        }
+        ed::SetCurrentEditor(nullptr);
+    }
+    ImGui::End();
 }
 
 void hantu::render(GLFWwindow* window) {
@@ -13,5 +40,5 @@ void hantu::render(GLFWwindow* window) {
 }
 
 void hantu::destroy() {
-
+    ed::DestroyEditor(editor);
 }
--- src/hantu.hxx
+++ src/hantu.hxx
@@ -1,6 +1,9 @@
 #include <layer.hxx>
+#include <imgui_node_editor.h>
 
 struct hantu : gui_layer {
+    ax::NodeEditor::EditorContext* editor = nullptr;
+
     bool demo_window_active = true;
 
     void init(GLFWwindow* window) override;
